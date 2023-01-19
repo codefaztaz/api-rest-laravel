8ac5911ff713584743775a1f1c1edd487ba4b9c8
codefaztaz <codefaztaz@gmail.com>
2023-01-19 22:06:22 +0000

    home and error components added and navbar on app.component.html created

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'inicio', component: HomeComponent },
+  { path: 'registro', component: RegisterComponent },
+  { path: '**', component: ErrorComponent }
+
+
+  
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
